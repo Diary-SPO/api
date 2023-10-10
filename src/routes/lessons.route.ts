aa9ac0df4de4f7b1f9efb5b11ff2c543241d246b
kopchan <kopchanus@gmail.com>
2023-10-11 01:27:57 +0700

    Create route lessons

---
+++ src/routes/lessons.route.ts
@@ -0,0 +1,10 @@
+import { Elysia } from 'elysia';
+import { type IMark } from 'diary-shared';
+//import { checkCookie } from '@src/middleware';
+
+const router = new Elysia()
+    .get('/:id/:startDate/:endDate', ({ params: { id, startDate, endDate } }) => {
+        // TODO: Сделать получение данных расписания
+    });
+
+export default router;
