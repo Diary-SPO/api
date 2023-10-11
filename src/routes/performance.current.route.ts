d0874663ef4f283abed77faa4728f4f250ae810a
kopchan <kopchanus@gmail.com>
2023-10-11 21:42:41 +0700

    Complited route performance.current

--- src/routes/performance.current.route.ts
+++ src/routes/performance.current.route.ts
@@ -1,10 +1,27 @@
-import { Elysia } from 'elysia';
-import { PerformanceCurrent } from 'diary-shared';
-//import { checkCookie, checkId } from '@src/middleware';
+import { Elysia } from 'elysia'
+import { type PerformanceCurrent } from 'diary-shared'
+// import { checkCookie, checkId } from '@src/middleware';
 
 const router = new Elysia()
-    .get('/:id', ({ params: { id } }) => {
-        // TODO: Сделать получение данных об недавных оценках
-    });
+  .get('performance.current/:id', async ({ request, set, params: { id } }) => {
+    const path = `${process.env.SERVER_URL}/services/reports/current/performance/${id}`
+    try {
+      const secret = request.headers.toJSON().secret
+      const response = await fetch(path, {
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          Cookie: secret
+        }
+      })
+      console.log(`${path}\t ${response.status}`)
+      const data = await response.json() as PerformanceCurrent
+      set.status = 200
+      return JSON.stringify(data)
+    } catch (e) {
+      console.error(`${path}\t failed:\n${e as string}`)
+      set.status = 500
+      return `Internal server error: ${e as string}`
+    }
+  })
 
-export default router;
+export default router
