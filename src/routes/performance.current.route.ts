9392a79890696b097be13a543c9dfd4c660ba111
kopchan <kopchanus@gmail.com>
2023-10-12 00:09:09 +0700

    Minor fixes

--- src/routes/performance.current.route.ts
+++ src/routes/performance.current.route.ts
@@ -1,11 +1,11 @@
 import { Elysia } from 'elysia'
-import { type PerformanceCurrent } from 'diary-shared'
-// import { checkCookie, checkId } from '@src/middleware';
+// import { type PerformanceCurrent } from 'diary-shared'
+// import { checkCookie, checkId } from '@src/middleware'
 
 const router = new Elysia()
   .get('performance.current/:id', async ({ request, set, params: { id } }) => {
-    const path = `${process.env.SERVER_URL}/services/reports/current/performance/${id}`
     try {
+      const path = `${process.env.SERVER_URL}/services/reports/current/performance/${id}`
       const secret = request.headers.toJSON().secret
       const response = await fetch(path, {
         headers: {
@@ -14,11 +14,11 @@ const router = new Elysia()
         }
       })
       console.log(`${path}\t ${response.status}`)
-      const data = await response.json() as PerformanceCurrent
+      const data = await response.json()
       set.status = 200
-      return JSON.stringify(data)
+      return data
     } catch (e) {
-      console.error(`${path}\t failed:\n${e as string}`)
+      console.error(`performance.current/:id\t failed:\n${e as string}`)
       set.status = 500
       return `Internal server error: ${e as string}`
     }
