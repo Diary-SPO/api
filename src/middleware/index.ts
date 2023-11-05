cd2600b40717fb61f3770e3893a1fcf9d7be9c8e
Kopchan <83940856+Kopchan@users.noreply.github.com>
2023-11-05 15:22:31 +0700

    chore: fix type (#10)
    * little fix
    
    * fix middleware
    
    ---------
    
    Co-authored-by: Kopchan <kopchanus@gmail.com>

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -17,10 +17,9 @@ interface CheckCookie {
 export const checkCookie = ({ request, set }: CheckCookie): number | undefined => {
   const secret = request.headers.get('secret')
 
-  if (!secret || secret === '') {
-    set.status = 401
-    return 401
+  if (secret) {
+    return
   }
-  
-  return
+  set.status = 401
+  return 401
 }
