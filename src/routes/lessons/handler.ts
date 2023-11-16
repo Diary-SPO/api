a46c11f723bef5a030e25d8997ba8597532c4eb0
scffs <khidey@inbox.ru>
2023-11-17 01:05:55 +0700

    refactor

--- src/routes/lessons/handler.ts
+++ src/routes/lessons/handler.ts
@@ -1,6 +1,6 @@
 import type { Day } from 'diary-shared'
 import type { Context } from 'elysia'
-import { HeadersWithCookie } from '@utils'
+import {adjustEndDate, formatDate, HeadersWithCookie} from '@utils'
 
 interface IContext extends Omit<Context, 'params'> {
   params: {
@@ -12,28 +12,17 @@ interface IContext extends Omit<Context, 'params'> {
 
 const getLessons = async ({ request, set, params }: IContext): Promise<Day[] | string> => {
   const { id, startDate, endDate } = params
-
-  const formattedStartDate = startDate.toString()
-  let formattedEndDate: string
-
-  formattedEndDate = endDate.toString()
-
-  const startTimestamp = new Date(startDate).getTime()
-  const endTimestamp = new Date(endDate).getTime()
-  const differenceInDays = (endTimestamp - startTimestamp) / (1000 * 3600 * 24)
-
-  if (differenceInDays > 14) {
-    const newEndDate = new Date(startTimestamp + 14 * 24 * 60 * 60 * 1000)
-    formattedEndDate = newEndDate.toISOString().substring(0, 10)
-  }
-
+  
+  const formattedStartDate = formatDate(startDate)
+  const formattedEndDate = adjustEndDate(startDate, endDate)
+  
   const secret = request.headers.toJSON().secret
   const path = `${process.env.SERVER_URL}/services/students/${id}/lessons/${formattedStartDate}/${formattedEndDate}`
-
+  
   const response = await fetch(path, {
     headers: HeadersWithCookie(secret)
   })
-
+  
   console.log(`${response.status} ${path}`)
   set.status = 200
   return await response.json()
---
+++ src/utils/formatted.ts
@@ -0,0 +1,37 @@
+/**
+ * Функция для форматирования даты в формате "YYYY-MM-DD".
+ * @param {string} date - Строка с датой.
+ * @returns {string} - Отформатированная дата.
+ */
+export const formatDate = (date: string): string => {
+  return new Date(date).toISOString().substring(0, 10)
+}
+
+/**
+ * Функция для вычисления разницы в днях между двумя датами.
+ * @param {string} start - Начальная дата.
+ * @param {string} end - Конечная дата.
+ * @returns {number} - Разница в днях.
+ */
+export const calculateDifferenceInDays = (start: string, end: string): number => {
+  const startTimestamp = new Date(start).getTime()
+  const endTimestamp = new Date(end).getTime()
+  return (endTimestamp - startTimestamp) / (1000 * 3600 * 24)
+}
+
+/**
+ * Функция для корректировки конечной даты, если разница в днях больше 14.
+ * @param {string} start - Начальная дата.
+ * @param {string} end - Конечная дата.
+ * @returns {string} - Откорректированная конечная дата.
+ */
+export const adjustEndDate = (start: string, end: string): string => {
+  const differenceInDays = calculateDifferenceInDays(start, end)
+  
+  if (differenceInDays > 14) {
+    const newEndDate = new Date(new Date(start).getTime() + 14 * 24 * 60 * 60 * 1000)
+    return formatDate(newEndDate.toISOString())
+  }
+  
+  return formatDate(end)
+}
