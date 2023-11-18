26ade153ce5cfbb5db28eb25d37bd3a518ca72a9
Zamelane <zamelane@vk.com>
2023-11-19 01:37:11 +0700

    Tsc

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { Elysia } from 'elysia'
 import { cors } from '@elysiajs/cors'
 import { helmet } from 'elysia-helmet'
+import { swagger } from '@elysiajs/swagger'
 
 import routes from '@routes'
 
 const port = process.env.PORT ?? 3003
 const app = new Elysia()
+  .use(swagger({
+    path: '/documentation'
+  }))
   .use(
     cors({
       origin: true,
