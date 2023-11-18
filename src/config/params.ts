8b6b7a9d380f986296244a9352c62c48e66ffa8a
scffs <khidey@inbox.ru>
2023-11-19 02:06:29 +0700

    improve types

--- src/config/params.ts
+++ src/config/params.ts
@@ -1,14 +1,19 @@
-import { ParamsInit, StringOrNumber } from './types'
+import type { NumericParams, ParamsInit, StringParams } from './types'
 
-const PARAMS_INIT: StringOrNumber<ParamsInit> = {
-  SERVER_URL: '',
+const NUMERIC_PARAMS: NumericParams = {
   PORT: 3003,
-  DATABASE_HOST: '',
   DATABASE_PORT: 5432,
+}
+
+const STRING_PARAMS: StringParams = {
+  SERVER_URL: '',
+  DATABASE_HOST: '',
   DATABASE_NAME: '',
   DATABASE_USERNAME: '',
   DATABASE_PASSWORD: '',
   ENCRYPT_KEY: '', // 32 символа
 }
+
+const PARAMS_INIT: ParamsInit = { ...NUMERIC_PARAMS, ...STRING_PARAMS }
 
 export default PARAMS_INIT
--- src/config/types.ts
+++ src/config/types.ts
@@ -6,17 +6,29 @@ export type StringOrNumber<T> = {
   [K in keyof T]: string | number
 }
 
-export interface ParamsInit {
-  SERVER_URL: string
+/**
+ * Утилитарный тип для извлечения ключей, соответствующих строковым значениям,
+ * из объекта типа T. Используется в контексте объектов с динамически заданными ключами.
+ * @template T - Объект, из которого извлекаются ключи.
+ */
+
+type StringKeys<T> = keyof {
+  [K in keyof T]: T[K] extends string ? K : never
+}
+
+export interface NumericParams {
   PORT: number
-  DATABASE_HOST: string
   DATABASE_PORT: number
+}
+
+export interface StringParams {
+  SERVER_URL: string
+  DATABASE_HOST: string
   DATABASE_NAME: string
   DATABASE_USERNAME: string
   DATABASE_PASSWORD: string
   ENCRYPT_KEY: string // 32 символа
 }
 
-export type ParamsKeys = keyof ParamsInit
-
-
+export type ParamsKeys = StringKeys<ParamsInit>
+export type ParamsInit = NumericParams & StringParams
