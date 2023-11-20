a6c2e158fdf5f801323a2713c2c49a6f446b32ff
scffs <khidey@inbox.ru>
2023-11-20 14:10:25 +0700

    chore: little fixes

--- src/types/database/index.ts
+++ src/types/database/index.ts
@@ -111,3 +111,16 @@ export interface Auth {
   token: string
   lastDate: string
 }
+
+export interface ResponseLogin {
+  id: number
+  spoId?: number
+  groupId: number
+  login: string
+  phone: string
+  birthday: string
+  firstName: string
+  lastName: string
+  middleName: string
+  token?: string
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,2 @@
 export * from './database'
-export * from './database/returnTypes'
 export * from './converterTypes'
