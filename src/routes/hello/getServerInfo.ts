c0119fb8dba0476932bc5643eca4b2734fdc5428
Zamelane <zamelane@vk.com>
2024-01-09 21:54:30 +0700

    Fix get commit hash

--- src/routes/hello/getServerInfo.ts
+++ src/routes/hello/getServerInfo.ts
@@ -1,11 +1,13 @@
 import { SERVER_URL } from '@config'
 import * as process from 'process'
+import {getGitCommitHash} from "./getGitCommitHash";
 
-export const getServerInfo = (): object => {
+export const getServerInfo = async (): Promise<object> => {
   return {
     status: 'ok',
     arch: process.arch,
     targetDiary: SERVER_URL,
-    backend: 'bun+elysia'
+    backend: 'bun+elysia',
+    commit: await getGitCommitHash() ?? 'noVersion'
   }
 }
